53d7ede2e3a5f235fe0b59c46b5c8a04e8216f89
kinno <y.kinno.sys23@morijyobi.ac.jp>
2024-06-01 14:17:59 +0900

    本の登録画面の作成

---
+++ src/main/java/jp/ac/morijyobi/book_management/controller/BookController.java
@@ -0,0 +1,19 @@
+package jp.ac.morijyobi.book_management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    @GetMapping("/register")
+    public String registerBook(Model model) {
+
+        return "book/register-book";
+    }
+
+
+}
